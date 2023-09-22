27f7ebbbe59f81e5ba6762ec72654b32caec13d5
Davy Jones <nikitayashin3@gmail.com>
2023-09-22 23:22:38 +0300

    make dir front

---
+++ frontend/src/shared/ui/Portal/Portal.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+interface PortalProps {
+  children: ReactNode;
+  element?: HTMLElement;
+}
+
+export const Portal: React.FC<PortalProps> = (props) => {
+  const { children, element = document.body } = props;
+  return createPortal(children, element);
+};
